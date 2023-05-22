0e5de930c01d21ac3b66fa8ed6f28023f56aba98
Jason Jody Aube <jasonjody2005@gmail.com>
2023-05-22 18:06:07 -0400

    TODO 15

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+//https://github.com/JasonJody/tpSynthese_H23p1116.git
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
-	// TODOs 15 : public static List<Personne> lireFichier(String fichier) 
+	// TODO 15 : public static List<Personne> lireFichier(String fichier)
     // TODO 16: public static void ecrireFichier(List<Personne> personnes, String fichier)
     public static class Personne {
         private String nom;
@@ -23,12 +29,11 @@ public class Main {
      
        
  
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // TODOs 11 à 14
         new CalculatriceSwing(); // démarre une calculatrice dont le fonctionnement et l'interface
                             // sont expliqués dans un clip: https://www.loom.com/share/b92c35aead684f3f872197a0558cb99a
-       
-        String fichier = "entrees.txt";  // fichier fourni
+       String fichier = "entrees.txt";
         // TODO 15
         List<Personne> personnes = lireFichier(fichier); // la classe Personne est fournie
         System.out.println("Données extraites du fichier :");
@@ -39,6 +44,34 @@ public class Main {
             System.out.println();
         }
         // TODO 16
-        ecrireFichier(personnes, "sorties.html"); // exemple fourni
+        //ecrireFichier(personnes, "sorties.html"); // exemple fourni
+
+
+    }
+
+    public static List<Personne> lireFichier(String cheminFichier) {
+        List<Personne> personnes = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(new File(cheminFichier));
+
+            while (scanner.hasNextLine()) {
+                String ligne = scanner.nextLine();
+                String[] informations = ligne.split(",");
+
+                String nom = informations[0].trim();
+                int age = Integer.parseInt(informations[1].trim());
+                String courriel = informations[2].trim();
+
+                Personne personne = new Personne(nom, age, courriel);
+                personnes.add(personne);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Le fichier n'a pas été trouvé : " + e.getMessage());
+        }
+
+        return personnes;
     }
 }
